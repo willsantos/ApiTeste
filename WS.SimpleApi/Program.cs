08f3688f4a01f11b7c40b6ea042aaa7af34de1ab
Alisson Demys <Alisson_demys@hotmail.com>
2023-12-22 10:55:55 -0300

    Fix http response to post method

--- WS.SimpleApi/Program.cs
+++ WS.SimpleApi/Program.cs
@@ -8,24 +8,32 @@ var app = builder.Build();
 var alunos = new List<Aluno>
 {
     new Aluno { Nome = "Ana", Idade = 20, Curso = "Engenharia", Turma = "A" },
-    new Aluno { Nome = "Bruno", Idade = 21, Curso = "Matemática", Turma = "B" },
+    new Aluno { Nome = "Bruno", Idade = 21, Curso = "Matemï¿½tica", Turma = "B" },
     new Aluno { Nome = "Carla", Idade = 19, Curso = "Biologia", Turma = "C" }
 };
-
-
 
 // Criar uma rota GET que retorna uma lista de alunos
 app.MapGet("/alunos", () => alunos);
 
 // Criar uma rota POST que adiciona um aluno na lista e retorna o JSON com os dados adicionados
-app.MapPost("/alunos", (Aluno novoAluno) =>
+app.MapPost("/alunos", async (HttpContext context) =>
 {
+    var random = new Random();
+    if (random.NextDouble() < 0.5)
+    {
+        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        await context.Response.WriteAsync("Erro interno no servidor.");
+        return;
+    }
+
+    var novoAluno = await context.Request.ReadFromJsonAsync<Aluno>();
     alunos.Add(novoAluno);
-    return novoAluno;
+
+    context.Response.StatusCode = StatusCodes.Status200OK;
+    await context.Response.WriteAsJsonAsync(novoAluno);
 });
 
 //TODO: rotas UPDATE E DELETE
-
 
 app.UseSwagger();
 app.UseSwaggerUI();
